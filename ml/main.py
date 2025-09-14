009db4b09d621133489053a57f1f7a02f91c2551
Damir <dense.neer@gmail.com>
2025-09-14 19:48:12 +0500

    ml models

--- ml/main.py
+++ ml/main.py
@@ -1,4 +1,15 @@
+from fastapi import FastAPI, File, UploadFile
+import cv2
+from ml.car_detect import cut_car_box
 
+app = FastAPI()
 
-# что тут ищещь?
-# в разработке
+@app.post("/api/car-checks/{id}/photos")
+def quality_check(file: File, data: dict):
+    frame = cv2.imread(file)
+    
+    car_img = cut_car_box(frame)
+    if car_img is None:
+        return {"error": "No car detected"}
+    
+    
